729bd2bd625e2ce96e6e52612d48bb5f9c85f0ca
MuWang <18640057101@163.com>
2024-04-11 20:48:32 +0800

    update multi chain

--- scripts/multi-protein/muti_protein_GMX.sh
+++ scripts/multi-protein/muti_protein_GMX.sh
@@ -1,16 +1,23 @@
 
-ORI_PDB="File.pdb"
+
+ORI_PDB="complex.pdb"
 
 gmx make_ndx -f $ORI_PDB -o index.ndx << INPUT
-a 1-100
-name 16 chain_A
-a 101-200
-name 17 chain_B
+keep 0
+a 187-2947
+name 1 chain_A
+a 2948-7958
+name 2 chain_B
+r GX1
+name 3 ligands_GX1
 q
 INPUT
 
-gmx editconf -f $ORI_PDB -n index.ndx -o chain_A.pdb -label A
-gmx editconf -f $ORI_PDB -n index.ndx -o chain_B.pdb -label B
+# new pdb start from atomid 1
+echo chain_A | gmx editconf -f $ORI_PDB -n index.ndx -o chain_A.pdb -label A
+echo chain_B | gmx editconf -f $ORI_PDB -n index.ndx -o chain_B.pdb -label B
+#echo ligands_GX1 | gmx editconf -f $ORI_PDB -n index.ndx -o chain_Z.pdb -label Z
 
-cat chain_A.pdb chain_B.pdb > combined.pdb
-gmx pdb2gmx -f combined.pdb -o combined.gro -p topol.top
+
+cat chain_*.pdb | grep ATOM > combined.pdb
+gmx pdb2gmx -f combined.pdb -o combined.gro -ignh -p  topol.top
